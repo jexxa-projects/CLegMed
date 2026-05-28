0c666f28d45f82a1eabc9274330e188a9a50a3a4
repplix <116941865+repplix@users.noreply.github.com>
2026-05-28 16:44:34 +0200

    * Added GTest Library for unit testing * updated HelloWorldTest to use gtest

---
+++ tests/core/HelloWorldTest.cpp
@@ -0,0 +1,15 @@
+#include <clegmed/core/library.h>
+#include <iostream>
+#include <format>
+#include <gtest/gtest.h>
+
+TEST(CoreTest, HelloWorldReturnsCorrectString) {
+    // 1. Arrange: Nothing
+
+    // 2. Act: Call the hello function from the shared library
+    std::string result = hello();
+    std::cout << std::format("[LOG] Erhaltene Rückgabe: '{}'\n", result);
+
+    EXPECT_EQ(result, "Hello World");
+    EXPECT_FALSE(result.empty());
+}
