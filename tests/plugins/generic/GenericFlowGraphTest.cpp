83b0a10323afed40d4102c5e843e4c20b52d811b
repplix <116941865+repplix@users.noreply.github.com>
2026-06-11 18:39:10 +0200

    feat(plugins/generic): added operator| to connect filter [REQ-004]
    Addresses: [REQ-004-A]
    Validated: GenericFlowGraphTest.cpp

--- tests/plugins/generic/GenericFlowGraphTest.cpp
+++ tests/plugins/generic/GenericFlowGraphTest.cpp
@@ -2,6 +2,7 @@
 // Created by Michael Repplinger on 31.05.26.
 //
 
+#include "clegmed/core/Pipe.hpp"
 #include "clegmed/core/Consumer.hpp"
 #include "clegmed/core/Processor.hpp"
 #include "clegmed/plugins/generic/GenericConsumer.hpp"
@@ -23,6 +24,29 @@ TEST(FlowGraphTest, GenericFlowGraphTest) {
     //Act
     producer.outputPipe().connect(processor.inputPipe());
     processor.outputPipe().connect(consumer.inputPipe());
+    producer.produce();
+
+    //Assert
+    EXPECT_EQ(data_storage.size(), 1);
+    EXPECT_EQ(data_storage[0], expected_result);
+}
+
+
+TEST(FlowGraphTest, PipeFlowGraphTest) {
+    //Arrange
+    using namespace clegmed::plugins::generic;
+
+    const auto expected_result = "Hello World";
+    std::vector<std::string> data_storage;
+
+    auto producer = emit("Hello");
+    auto processor = append(" World");
+    auto consumer = store(data_storage);
+
+    //Act
+    producer | processor | consumer;
+
+    //Act
     producer.produce();
 
     //Assert
