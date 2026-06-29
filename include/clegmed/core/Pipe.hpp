665c227745576bfeed87dbb45e30bbd38eb0a201
repplix <116941865+repplix@users.noreply.github.com>
2026-06-29 23:09:11 +0200

    refactor(core): extract ConnectablePipes concept from operator|  [CHORE-001]

--- include/clegmed/core/Pipe.hpp
+++ include/clegmed/core/Pipe.hpp
@@ -2,12 +2,14 @@
 #include <utility>
 
 namespace clegmed::core {
-
     template<typename TLeft, typename TRight>
-    requires requires(TLeft& l, TRight& r) {
+       concept ConnectablePipes = requires(TLeft& l, TRight& r) {
         l.outputPipe();
         r.inputPipe();
-    }
+       };
+
+    template<typename TLeft, typename TRight>
+    requires ConnectablePipes<TLeft, TRight>
     decltype(auto) operator|(TLeft&& left, TRight&& right) { //NOSONAR - left is not used for forwarding
         left.outputPipe().connect(right.inputPipe());
         return std::forward<TRight>(right);
