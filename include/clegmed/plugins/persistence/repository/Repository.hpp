0047835a9d3b753d8861ff3cd58155a76cbd3727
repplix <116941865+repplix@users.noreply.github.com>
2026-07-08 15:29:03 +0200

    fix(plugin): Corrected concept of Repository so that add uses foward instead of move  [REQ-008]

--- include/clegmed/plugins/persistence/repository/Repository.hpp
+++ include/clegmed/plugins/persistence/repository/Repository.hpp
@@ -23,7 +23,7 @@ namespace clegmed::plugins::persistence {
             { repository.update( std::move(entity_handle) ) }  -> std::same_as<void>;
             { repository.remove(entity_id) }        -> std::same_as<void>;
             { repository.removeAll() }              -> std::same_as<void>;
-            { repository.add(std::move(entity)) }   -> std::same_as<void>;
+            { repository.add(std::forward<Entity>(entity)) }   -> std::same_as<void>;
             { repository.get( entity_id )}          -> std::same_as<std::optional<utils::EntityHandle_t<Entity>>>;
             { repository.getAll()}                  -> std::ranges::range;
         };
